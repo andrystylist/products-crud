213616c95db55fcc9ecb23b9c10e0ffc4816511a
Andry <andry.carolina88@gmail.com>
2023-07-06 22:03:36 -0500

    hardcode the Array of categories

--- src/services/Category.js
+++ src/services/Category.js
@@ -1,9 +1,27 @@
-import host from './host'
 export default class Category {
   static async getCategories () {
-    const response = await fetch(`${host}/categories`)
-    const jsonResponse = await response.json()
-
-    return Array.isArray(jsonResponse) ? jsonResponse : []
+    const categories = [
+      {
+        id: 1,
+        name: 'Music'
+      },
+      {
+        id: 2,
+        name: 'Clothing'
+      },
+      {
+        id: 3,
+        name: 'Home'
+      },
+      {
+        id: 4,
+        name: 'Baby'
+      },
+      {
+        id: 5,
+        name: 'Books'
+      }
+    ]
+    return categories
   }
 }
